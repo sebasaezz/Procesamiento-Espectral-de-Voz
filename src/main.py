37d45acb5844d9fe5d87914e1f1e50c1f2200478
sebasaezz <sebasaez2005@gmail.com>
2026-05-25 18:03:36 -0400

    Espectrograma funcionando :)

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from audio_io import load_audio
 from dft import dividir_en_bloques, aplicar_ventana, calcular_dft
+from plots import plot_espectrograma
 
 u_s, x, file_path =  load_audio()
 
-N = 2000
-hop = 200
+N = 2048
+hop = 1024
 
 bloques = dividir_en_bloques(x, N, hop)
 
-bloques_ventana = aplicar_ventana(bloques, N)
-calcular_dft(bloques_ventana)
+bloques_ventana, ventana = aplicar_ventana(bloques, N)
+X = calcular_dft(bloques_ventana)
+
+plot_espectrograma(X, N, hop, u_s)
--- src/plots.py
+++ src/plots.py
@@ -1,7 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_espectrograma(X):
-    M = np.abs(X)
-    M = M[0 ::]
-    pass
+def plot_espectrograma(X, N, hop, u_s):
+    S = np.abs(X)
+    k_n = N//2
+    S = S[:, : k_n+1]
+    S = 20*np.log10(S)
+    S = np.transpose(S)
+    S = np.flip(S, axis=0)
+
+    n_bloques = X.shape[0]
+    T_s = 1/u_s
+    t_max = T_s*((n_bloques)*hop + N/2)
+    plt.figure(figsize=(10,5))
+    plt.imshow(S, aspect="auto", extent=(T_s*N/2, t_max, 0, u_s/2))
+    plt.xlabel("Tiempo [s]")
+    plt.ylabel("Frecuencia [Hz]")
+    plt.yscale("symlog", linthresh=100)
+    plt.show()
