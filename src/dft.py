8620b0cd851c9a5c6cea241b170e00c94c33a696
sebasaezz <sebasaez2005@gmail.com>
2026-05-25 02:24:34 -0400

    Ventanas listas

--- src/dft.py
+++ src/dft.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal.windows import gaussian
 
 def dividir_en_bloques(x, N, hop):
     if len(x) < N:
@@ -14,3 +15,41 @@ def dividir_en_bloques(x, N, hop):
     
     return np.array(bloques)
 
+def aplicar_ventana(bloques, N):
+    ventana = escojer_ventana(N)
+
+    return bloques*ventana, ventana
+
+def escojer_ventana(N):
+    ventana = ""
+
+    opciones = ["Sin ventana", "Gauss", "Triangulo", "Hanning", "Hamming", "Blackman"]
+
+    print("Escoja una ventana a usar:")
+    for i, opcion in enumerate(opciones):
+        print("   ", f"[{i+1}]", opcion)
+
+    while True:
+        try:
+            numero_elegido = int(input("Escoja un número: "))
+        except ValueError:
+            print("Asegúrese de escribir un número")
+            continue
+        if numero_elegido in range(1,7):
+            ventana = opciones[numero_elegido-1]
+            break
+        else:
+            print("Elección fuera de rango, intente de nuevo")
+
+    if ventana == "Sin ventana":
+        return 1
+    elif ventana == "Gauss":
+        return gaussian(N, std=N/6)
+    elif ventana == "Triangulo":
+        return np.bartlett(N)
+    elif ventana == "Hanning":
+        return np.hanning(N)
+    elif ventana == "Hamming":
+        return np.hamming(N)
+    elif ventana == "Blackman":
+        return np.blackman(N)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from audio_io import load_audio
-from dft import dividir_en_bloques
+from dft import dividir_en_bloques, escojer_ventana
 
 u_s, x, file_path =  load_audio()
 
 b = dividir_en_bloques(x, 100, 50)
 
-print(b)
+escojer_ventana()
